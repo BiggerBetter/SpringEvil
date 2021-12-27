9cdf3b2a5e795d6fbfdf86e894ca92329df595a2
BiggerBetter <jiong23@qq.com>
2021-12-27 23:34:49 +0800

    新SpringEvil

---
+++ src/main/java/com/jenius/springevil/SpringEvilApplication.java
@@ -0,0 +1,13 @@
+package com.jenius.springevil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEvilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEvilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenius/springevil/SpringEvilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenius.springevil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEvilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
